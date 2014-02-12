f177be236ab13c08a7c0f7dd2a7096884c7819da
Erik Farmer <erikdfarmer@gmail.com>
2014-02-12 12:07:10 -0800

    add heroku deployment changes

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,5 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-ApiDemo::Application.config.secret_token = '343d4a059bb6b8931e798a55c727e744a4ae3e3c0fe4ba92294a63000ee898026cb8ee5a95b2b9eee0ee13804e50c97dd73cd686f5063c97cea7aa931edbfbcc'
+ApiDemo::Application.config.secret_token = if Rails.env.development? or Rails.env.test?
+  ('x' * 30)
+else
+  ENV['SECRET_TOKEN']
+end
